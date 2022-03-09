7870d3f50476eadec2e40cd533ca4bf8c004d197
FadillaRatnaDwita29RPL <fadilladwita20@gmail.com>
2022-03-09 09:16:30 +0700

    first commit

---
+++ src/Alert.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Alert extends Component {
+    render() {
+        return (
+            // <div className="alert alert-success">
+            //     <h4>Info</h4>
+            //     Belajar React JS itu menyenangkan
+            // </div>
+
+            <div className={"alert alert-" + this.props.type}>
+                <h4>{this.props.header}</h4>
+                {this.props.children}
+            </div>
+
+        )
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Alert from './Alert'
+import Media from './Media'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      type: "info",
+      header: "Modul 1 React JS",
+      content: "Alert"
+    }
+  }
+
+  // fungsi untuk mengubah tipe  
+  changeTypeAlert = (event) => {
+    this.setState({ type: event.target.value });
+  }
+  // fungsi untuk mengubah header  
+  changeHeaderAlert = (event) => {
+    this.setState({ header: event.target.value });
+  }
+  // fungsi untuk mengubah content  
+  changeContentAlert = (event) => {
+    this.setState({ content: event.target.value });
+  }
+
+
+  render() {
+    return (
+      <div >
+      <div className="App ">
+        <h3 className='judul'>Ini Project Pertama React JS</h3>
+        <p className='subJudul'>Belajar React JS itu mudah</p>
+      </div><br/>
+
+       <div className="App container">  
+         <div className="alert alert-info">  
+           <h3 className="text-danger">Ini Project Pertama React JS</h3>  
+           <p>Belajar React JS itu mudah</p>  
+           <button className="mr-1 btn btn-success">Setuju</button>  
+           <button className="btn btn-info">Iya Dong</button>  
+         </div>  
+       </div><br/>
+
+         <div className="App container col-sm-6">  
+         <Alert />  
+         <Alert />  
+         <Alert />  
+       </div> <br/> 
+
+       <div className="App container col-sm-6">  
+           <Alert type="danger" header="Fatal Error">  
+             Ini adalah alert dengan tipe danger  
+           </Alert>  
+           <Alert type="success" header="Berhasil">  
+             Ini adalah alert dengan tipe success  
+           </Alert>  
+           <Alert type="warning" header="Warning">  
+             Ini adalah alert dengan tipe warning  
+           </Alert>  
+         </div><br/> 
+
+      <div className="col-sm-6">
+        <Media image="logo192.png" title="React JS">
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+          scelerisque ante sollicitudin. Cras purus odio, vestibulum
+          in vulputate at, tempus viverra turpis
+        </Media>
+        <Media image="angular.png" title="Angular JS">
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+          scelerisque ante sollicitudin. Cras purus odio, vestibulum
+          in vulputate at, tempus viverra turpis
+        </Media>
+      </div><br/>
+      
+      <div className="App container col-sm-6">
+        <Alert type={this.state.type} header={this.state.header}>
+          {this.state.content}
+        </Alert>
+        <hr />
+        <h4>Alert Control</h4>
+        <b className="text-left">Tipe Alert</b>
+        <select className="form-control" name="type" value={this.state.type}
+          onChange={this.changeTypeAlert}>
+          <option value="success">Success</option>
+          <option value="warning">Warning</option>
+          <option value="danger">Danger</option>
+          <option value="info">Info</option>
+        </select>
+        <b className="text-left">Header Alert</b>
+        <input
+          type="text" name="header" className="form-control" value={this.state.header}
+          onChange={this.changeHeaderAlert} />
+        <b className="text-left">Content Alert</b>
+        <input
+          type="text" name="content" className="form-control" value={this.state.content}
+          onChange={this.changeContentAlert} />
+      </div><br/><br/>
+      </div>
+    )
+  }
 }
-
-export default App;
